064e107e531e44771d61ba5d6eba1d6615d90103
sam43125 <sam43125.cs06@g2.nctu.edu.tw>
2019-03-29 11:20:52 +0800

    Correct encryption of PKCS mode.

--- AES/aes-modes.cpp
+++ AES/aes-modes.cpp
@@ -27,28 +27,16 @@ using CryptoPP::StringSink;
 using CryptoPP::StringSource;
 using CryptoPP::StreamTransformationFilter;
 
-//#define bytelen(x) (sizeof(x))/(sizeof(byte))
 using CryptoPP::byte;
-
-//string PKCS7_PADDING(string strIn) {
-//    string strOut = strIn;
-//    size_t padNum = AES::BLOCKSIZE - strIn.length() % AES::BLOCKSIZE;
-//    for (size_t i = 0; i < padNum; i++) {
-//        strOut += padNum;
-//    }
-//    if (padNum == 0)
-//        strOut += string(AES::BLOCKSIZE, AES::BLOCKSIZE);
-//    return strOut;
-//}
 
 enum Padding {
     Z, P
 };
 
-const string ECBEncrypt(const byte* plain, size_t len, const byte* key, Padding p) {
+const string ECBEncrypt(const string& plain, const byte* key, Padding p) {
     string cipher, encoded;
     ECB_Mode<AES>::Encryption e(key, AES::DEFAULT_KEYLENGTH);
-    StringSource(plain, len, true,
+    StringSource(plain, true,
         new StreamTransformationFilter(
             e,
             new StringSink(cipher),
@@ -94,10 +82,10 @@ const string ECBDecrypt(const string& cipher, const byte* key, Padding p, bool i
     return recovered;
 }
 
-const string CBCEncrypt(const byte* plain, size_t len, const byte* key, const byte* iv, Padding p) {
+const string CBCEncrypt(const string& plain, const byte* key, const byte* iv, Padding p) {
     string cipher, encoded;
     CBC_Mode<AES>::Encryption e(key, AES::DEFAULT_KEYLENGTH, iv);
-    StringSource(plain, len, true,
+    StringSource(plain, true,
         new StreamTransformationFilter(
             e,
             new StringSink(cipher),
@@ -144,16 +132,6 @@ const string CBCDecrypt(const string& cipher, const byte* key, const byte* iv, P
 }
 
 
-const byte* str2bytes(const string& str) {
-    // Padding 0x00 to result
-    byte* result = new byte[str.length() + 1];
-    size_t i = 0;
-    for (; i < str.length(); i++)
-        result[i] = str[i];
-    result[i] = 0x00;
-    return result;
-}
-
 #ifndef NO_MAIN
 
 int main(int argc, char* argv[]) {
@@ -169,25 +147,21 @@ int main(int argc, char* argv[]) {
     copy(b, b + sizeof(b) - 1, iv);
 
     const string plain = "AES is efficient in both software and hardware.";
-    // 41455320697320656666696369656e7420696e20626f746820736f66747761726520616e642068617264776172652e00
+    // 41455320697320656666696369656e7420696e20626f746820736f66747761726520616e642068617264776172652e
     string encoded;
 
-    const byte* bplain = str2bytes(plain);
-    size_t len = plain.length() + 1;
-
-    encoded = ECBEncrypt(bplain, len, key, Z);
+    encoded = ECBEncrypt(plain, key, Z);
     fout << encoded.c_str() << endl << endl;
 
-    encoded = ECBEncrypt(bplain, len, key, P);
+    encoded = ECBEncrypt(plain, key, P);
     fout << encoded.c_str() << endl << endl;
 
-    encoded = CBCEncrypt(bplain, len, key, iv, Z);
+    encoded = CBCEncrypt(plain, key, iv, Z);
     fout << encoded.c_str() << endl << endl;
 
-    encoded = CBCEncrypt(bplain, len, key, iv, P);
+    encoded = CBCEncrypt(plain, key, iv, P);
     fout << encoded.c_str() << endl << endl;
 
-    delete[] bplain;
     fout.close();
     return 0;
 }
